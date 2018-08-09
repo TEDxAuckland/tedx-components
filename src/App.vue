19bc6ae9b9df8defbf17f781946c5c6e9c155f6f
Anthony <anthony@anthonymace.com>
2018-08-09 16:23:10 +1200

    full width content when no video

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,9 @@ import HelloWorld from './sections/HelloWorld.vue'
 import Speakers from './sections/Speakers.vue'
 
 import speakerJson from './assets/json/people.json'
+var speakerNames = ["andy-crowe--rui-peng","alexander-macdonald","angie-judge","anna-faber","cameron-sims","cather-simpson","chris-pemberton","david-razowsky","emeline-paat-dahlstrom","jahra-rager-wasasala","juliet-arnott","liam-malone","lisa-king","mai-chen","michael-corballis","nigel-parker","pete-bethune","sam-stubbs","shaveer-mirpuri","william-watson","yoseph-ayele"]
 // var speakerNames = ["barbara-breen","ian-mccrae","lizzie-marvelly","jayne-bailey","cori-gonzalez-macuer","adrien-taylor","mike-moka","anna-coddington","toby-carr","james-bergin","vaughan-rowsell","grace-clapham--solonia-teodros","jess-holly-bates","samuel-gibson","georgia-lala","richard-aston","pani-farvid","sharad-paul","minnie-barragwanath","richard-little","david-harvey","matt-shirtcliffe","keith-ng","rory-steyn","modern-māori-quartet"]
-var speakerNames = ["siouxsie-wiles", "grant-schofield", "riley--steve-hathaway", "steve-pointing", "hong-sheng-chiong", "tom-scott", "gavin-healy", "billie-jordan", "the-hip-op-eration-crew", "dale-pfeifer", "janette-searle", "sir-bob-harvey", "tame-iti", "max-cryer"]
+// var speakerNames = ["siouxsie-wiles", "grant-schofield", "riley--steve-hathaway", "steve-pointing", "hong-sheng-chiong", "tom-scott", "gavin-healy", "billie-jordan", "the-hip-op-eration-crew", "dale-pfeifer", "janette-searle", "sir-bob-harvey", "tame-iti", "max-cryer"]
 
 
 export default {
--- src/components/ExpandedPreview.vue
+++ src/components/ExpandedPreview.vue
@@ -2,7 +2,7 @@
   <section class="full-width-section">
     <div class="expanded-preview__wrapper">
       <div class="container expanded-preview">
-        <div class="expanded-preview__video">
+        <div v-if="speaker.youtube_video_id" class="expanded-preview__video">
           <template v-if="speaker.youtube_video_id">
             <iframe
               :src="'https://www.youtube-nocookie.com/embed/' + speaker.youtube_video_id + '?rel=0&amp;showinfo=0'"
@@ -12,7 +12,10 @@
             </iframe>
           </template>
         </div>
-        <div class="expanded-preview__content">
+        <div
+          class="expanded-preview__content"
+          :class="{'expanded-preview__content--full-width': !speaker.youtube_video_id }"
+          >
           <div class="expanded-preview__header">
             <h1 class="person__title">{{ speaker.name }}</h1>
             <a :href="speaker.url" class="expanded-preview__profile-link"> see profile...</a>
