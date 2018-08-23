2db1d4ac4aa3670d5c84fb36a6700b36360df4c8
Anthony <anthony@anthonymace.com>
2018-08-23 13:18:11 +1200

    run vue add router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import AppGif from './AppGif.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(AppGif)
 }).$mount('#app')
