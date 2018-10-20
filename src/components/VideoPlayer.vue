4e7c627b27013b3326c28ddd1f79bed58460ecbe
David Staehler <david.staehler@googlemail.com>
2018-10-20 21:03:54 +1300

    Add video player component and video player view

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="video-player">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "video-player",
+  props: ['videoId']
+}
+
+</script>
+
+<style lang="scss">
+</style>
