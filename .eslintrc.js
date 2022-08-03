fad1a0f4fb2ad85f152368cfabb8f958e58efc4c
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-08-03 21:33:46 +1200

    snake grid works

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-inner-declarations': 'off',
+    'no-constant-condition': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/components/EventsSnakeGrid.vue
+++ src/components/EventsSnakeGrid.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="carousel">
     snake grid goes here
+    <!-- <pre class="content" v-html="template.areas.join(' ')"></pre> -->
+    <div :style="{ 
+      'grid-template-areas': template.areas.join(' '),
+      'display': 'grid',
+      'grid-auto-columns': 100 + 'px',
+      'grid-auto-rows': 100 + 'px',
+      'gap': 20 + 'px'
+    }">
+    
+      <div v-for="item in items" :key="item.id" :style="{
+        'grid-area': `item-${item.id}`,
+        'background-color': 'grey'
+      }">
+        {{ item.id }}
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import {
-  Hooper,
-  Slide,
-  Navigation as HooperNavigation,
-  Pagination as HooperPagination
-} from 'hooper'
-import 'hooper/dist/hooper.css'
+// import {
+//   Hooper,
+//   Slide,
+//   Navigation as HooperNavigation,
+//   Pagination as HooperPagination
+// } from 'hooper'
+// import 'hooper/dist/hooper.css'
+import {getGridTemplate} from './eventsSnakeGrid'
 
 export default {
   name: "carousel",
   components: {
-    Hooper,
-    Slide,
-    HooperNavigation,
-    HooperPagination
+    // Hooper,
+    // Slide,
+    // HooperNavigation,
+    // HooperPagination
   },
   props: {
-    images: {
+    items: {
       type: Array,
     }
   },
   data() {
     return {
+    }
+  },
+  computed: {
+    template() {
+      return getGridTemplate(4, this.items)
     }
   },
   methods: {},
