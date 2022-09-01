f55098af10991d3d2a84c004942fa4e60b6679ed
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-09-01 19:44:44 +1200

    tidy up

--- src/views/EventCardBig.vue
+++ src/views/EventCardBig.vue
@@ -98,8 +98,6 @@ export default {
 
 .event-card-big__header {
   display: block;
-  margin-block-start: 0.67em;
-  margin-block-end: 0.67em;
   margin-inline-start: 0px;
   margin-inline-end: 0px;
   font-weight: bold;
