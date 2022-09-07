ce9a097071e4c4e9f1f66d70bc950478209ec4c5
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-09-07 18:39:18 +1200

    add events-snake-grid-responsive component

---
+++ src/views/EventsSnakeGridResponsive.vue
@@ -0,0 +1,69 @@
+<template>
+  <EventsSnakeGrid
+    :items="items"
+    :lineColor="lineColor"
+    :cellSize="(containerWidth - dynamicGap * 3) / 4"
+    :gap="dynamicGap"
+    v-slot="{ item, cellSize, gap }"
+  >
+    <EventCard
+      :item="item"
+      :cellSize="cellSize"
+      :gap="gap" 
+    />
+  </EventsSnakeGrid>
+</template>
+
+<script>
+import EventsSnakeGrid from '@/components/EventsSnakeGrid.vue'
+import EventCard from '../components/EventCard'
+
+export default {
+  name: 'events-snake-grid-responsive',
+
+  components: {
+    EventsSnakeGrid,
+    EventCard
+  },
+
+  methods: {
+    resized() {
+      this.gridWidth = window.innerWidth
+    }
+  },
+  mounted() {
+    window.addEventListener("resize", this.resized);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.resized);
+  },
+  props: {
+    items: {
+      type: Array,
+    },
+    lineColor: {
+      type: String,
+      default: '#000',
+    },
+  },
+
+  data() {
+    return {
+      gridWidth: window.innerWidth
+    }
+  },
+
+  computed: {
+    containerWidth() {
+      return Math.min(this.gridWidth, 1200)
+    },
+    dynamicGap() {
+      return this.containerWidth * 11 / 120
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
